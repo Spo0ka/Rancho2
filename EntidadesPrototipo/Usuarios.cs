c27ee3dc01035c5f5b4ed5a5f72b90fee4161292
Alfredowo <112134780+Alfredowo@users.noreply.github.com>
2023-04-20 09:18:41 -0600

    se agrego y programo la entidad usuarios

---
+++ EntidadesPrototipo/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesPrototipo
+{
+    public class Usuarios
+    {
+        public Usuarios(int id, string nombre, string apellido, string nombreUsuario, string contraseña, string tipo)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            NombreUsuario = nombreUsuario;
+            Contraseña = contraseña;
+            Tipo = tipo;
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Tipo { get; set; }
+    }
+}
